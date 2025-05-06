b4f3794f3fdf8f5dcb15dfbfaf72b773e266845c
zatvornik <laamiritodehabirito@gmail.com>
2025-05-06 17:52:25 +0300

    Remove all files

--- greenclip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-greenclip print | grep . | dmenu -i -l 10 \
--nb '#000000' -nf '#FFFFFF' \
--sb '#000000' -sf '#00FF00' \
--fn 'FiraCode-15:bold' | xargs -r -d'\n' -I '{}' greenclip print '{}'
